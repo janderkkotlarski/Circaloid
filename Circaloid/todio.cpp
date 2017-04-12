03e78010ac4fd97e4258cc4e9738c9db0c436562
janderkkotlarski <j.d.kotlarski@gmx.com>
2017-04-12 12:34:58 +0200

    more sounds

--- Circaloid/todio.cpp
+++ Circaloid/todio.cpp
@@ -1,1 +1,10 @@
 #include "todio.h"
+
+Todio::Todio()
+    : m_names({"Diur.wav"}),
+      m_buffers(),
+      m_sounds()
+{
+    // for(std::)
+
+}
--- Circaloid/todio.h
+++ Circaloid/todio.h
@@ -2,12 +2,20 @@
 #define TODIO_H
 
 #include <cassert>
+#include <string>
+#include <vector>
 
 #include "SFML/Audio.hpp"
 
 class Todio
 {
+    std::vector <std::string> m_names;
+    std::vector <sf::SoundBuffer> m_buffers;
+    std::vector <sf::Sound> m_sounds;
 
+public:
+
+    Todio();
 };
 
 #endif // TODIO_H
