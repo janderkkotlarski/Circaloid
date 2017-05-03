a73111e824ea44ac3f362a5226af8ef23fe7a237
janderkkotlarski <j.d.kotlarski@gmx.com>
2017-05-03 12:27:11 +0200

    more cleansing

--- Circaloid/game.h
+++ Circaloid/game.h
@@ -20,25 +20,30 @@ class Game
     std::vector <std::string> init_names();
     std::vector <std::vector <sf::Keyboard::Key>> init_keybindings();
 
-    std::vector <Tohoid> init_tohoids(const sf::Vector2f &windims,
-                                      const std::vector <sf::Vector2f> &posits,
-                                      const std::vector <float> &rotats,
-                                      const std::vector <std::string> &names,
-                                      const std::vector <std::vector <sf::Keyboard::Key>> &keys, const float frame);
+    std::vector <Tohoid> init_tohoids(const sf::Vector2f& windims,
+                                      const std::vector <sf::Vector2f>& posits,
+                                      const std::vector <float>& rotats,
+                                      const std::vector <std::string>& names,
+                                      const std::vector <std::vector <sf::Keyboard::Key>>& keys,
+                                      const float frame);
 
-    void touhous_die(std::vector <Tohoid> &touhous);
+    void touhous_die(std::vector <Tohoid>& touhous);
 
-    bool game_loop(sf::RenderWindow &window, const sf::Color &background, const sf::Vector2f &windims, std::vector<Tohoid> &touhous, const float frame);
+    bool game_loop(sf::RenderWindow& window,
+                   const sf::Color& background,
+                   const sf::Vector2f& windims,
+                   std::vector<Tohoid> & touhous,
+                   const float frame);
 
 public:
 
     explicit Game();
     ~Game();
 
-    void run(sf::RenderWindow &window, const sf::Vector2f &windims, const sf::Color &background,
-             const float frame, const int amount, bool &nope, const std::vector<std::string> &touhou_names);
+    void run(sf::RenderWindow& window, const sf::Vector2f& windims, const sf::Color& background,
+             const float frame, const int amount, bool& nope, const std::vector<std::string>& touhou_names);
 };
 
-std::vector <sf::Keyboard::Key> chars2keys(const std::vector <char> &charas);
+std::vector <sf::Keyboard::Key> chars2keys(const std::vector <char>& charas);
 
 #endif // GAME_H
